cd2d63eb803f71b93827f2ada12585dd775f92dd
juancgalueweb <juancgalue@icloud.com>
2023-02-07 00:54:45 -0300

    Fix a bug in the Footer

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -1,4 +1,3 @@
-import { Footer } from 'flowbite-react'
 import {
   AiFillGithub,
   AiFillLinkedin,
@@ -8,35 +7,49 @@ import {
 
 const FooterApp = () => {
   return (
-    <Footer container={true} className='border-t dark:border-none'>
-      <Footer.Copyright by='Juan Carlos Galué' year={2023} />
-      <Footer.LinkGroup>
-        <Footer.Link
-          href='https://www.linkedin.com/in/juancgalue/'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <AiFillLinkedin className='text-[#0B66C2] text-3xl mr-1' />
-        </Footer.Link>
-        <Footer.Link
-          href='https://github.com/juancgalueweb'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <AiFillGithub className='text-black dark:text-white text-3xl mx-1' />
-        </Footer.Link>
-        <Footer.Link
-          href='https://twitter.com/juancgalue'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <AiFillTwitterCircle className='text-[#1DA1F2] text-3xl mx-1' />
-        </Footer.Link>
-        <Footer.Link href='mailto: juancgalue@gmail.com?subject=Correo%20desde%20Portafolio%20de%20Juan%20Galue'>
-          <AiOutlineMail className='text-3xl mx-1' />
-        </Footer.Link>
-      </Footer.LinkGroup>
-    </Footer>
+    <footer className='p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between dark:bg-gray-800 border-t dark:border-none'>
+      <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+        &copy; 2023 Juan Carlos Galu&eacute;
+      </span>
+      <ul className='flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0'>
+        <li>
+          <a
+            href='https://www.linkedin.com/in/juancgalue/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <AiFillLinkedin className='text-[#0B66C2] text-3xl mr-4 md:mr-6' />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://github.com/juancgalueweb'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <AiFillGithub className='text-black dark:text-white text-3xl mr-4 md:mr-6' />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://twitter.com/juancgalue'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <AiFillTwitterCircle className='text-[#1DA1F2] text-3xl mr-4 md:mr-6' />
+          </a>
+        </li>
+        <li>
+          <a
+            href='mailto: juancgalue@gmail.com?subject=Correo%20desde%20Portafolio%20de%20Juan%20Galue'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <AiOutlineMail className='text-3xl' />
+          </a>
+        </li>
+      </ul>
+    </footer>
   )
 }
 
--- src/pages/myprojects/index.tsx
+++ src/pages/myprojects/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next'
 import AppWrapper from '../components/AppWrapper'
 import Portfolio from '../components/Portfolio'
+import ScrollToTop from '../components/ScrollToTop'
 
 const Projects: NextPage = () => {
   const title = 'Mis proyectos web'
@@ -9,6 +10,7 @@ const Projects: NextPage = () => {
 
   return (
     <AppWrapper title={title} description={description}>
+      <ScrollToTop />
       <Portfolio />
     </AppWrapper>
   )
