c538686921fda3df896f7fd6b2c12dac832fd0dd
juancgalueweb <juancgalue@icloud.com>
2023-02-17 16:22:22 -0300

    Animate the toogle theme button in the Navbar

--- src/services/ThemeSwitch.tsx
+++ src/services/ThemeSwitch.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
 import { BsBrightnessHighFill, BsFillMoonStarsFill } from 'react-icons/bs'
 
@@ -37,13 +38,35 @@ const ThemeSwitch = () => {
   // process.browser is deprecated can be written as typeof window === 'undefined'
   return (
     <div
-      className='border-2 rounded-md border-indigo-500 bg-indigo-500 hover:border-indigo-700 hover:bg-indigo-700 dark:border-amber-200 dark:bg-amber-200 dark:hover:bg-amber-400 dark:hover:border-amber-400 p-2 transform transition duration-300 hover:cursor-pointer mx-2'
+      className='border-2 rounded-md border-indigo-500 bg-indigo-500 hover:border-indigo-700 hover:bg-indigo-700 dark:border-amber-200 dark:bg-amber-200 dark:hover:bg-amber-400 dark:hover:border-amber-400 p-2 transform transition duration-300 hover:cursor-pointer mx-2 flex items-center'
       onClick={toggleMode}
     >
       {darkModeActive ? (
-        <BsBrightnessHighFill className='text-xl cursor-pointer dark:text-[#1F2937]' />
+        <AnimatePresence mode='wait' initial={false}>
+          <motion.div
+            style={{ display: 'inline-block' }}
+            key={darkModeActive ? 'dark' : 'light'}
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 20, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            <BsBrightnessHighFill className='text-xl cursor-pointer dark:text-[#1F2937]' />
+          </motion.div>
+        </AnimatePresence>
       ) : (
-        <BsFillMoonStarsFill className='text-xl cursor-pointer text-slate-100' />
+        <AnimatePresence mode='wait' initial={false}>
+          <motion.div
+            style={{ display: 'inline-block' }}
+            key={darkModeActive ? 'dark' : 'light'}
+            initial={{ y: -20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 20, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            <BsFillMoonStarsFill className='text-xl cursor-pointer text-slate-100' />
+          </motion.div>
+        </AnimatePresence>
       )}
     </div>
   )
