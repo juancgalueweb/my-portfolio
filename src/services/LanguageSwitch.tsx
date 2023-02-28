51d73814c8f749f95a7f68920eda87a28932a9e4
juancgalueweb <juancgalue@icloud.com>
2023-02-28 19:38:31 -0300

    Revert "Get the Tooltip out of the Link. Triggers the Tooltip while hover"
    This reverts commit 24dd89434eb22afaa7cb20da7abe32ab6132fbbd.

--- src/services/LanguageSwitch.tsx
+++ src/services/LanguageSwitch.tsx
@@ -12,37 +12,29 @@ const LanguageSwitch = () => {
   const restOfLocales = locales && locales.filter(l => l !== locale)
 
   return (
-    <>
+    <Link
+      href={CheckValidPath(pathname)}
+      locale={restOfLocales && restOfLocales[0]}
+      scroll={false}
+    >
       {locale === 'es' ? (
-        <Tooltip content='es → en' placement='bottom' trigger='hover'>
-          <Link
-            href={CheckValidPath(pathname)}
-            locale={restOfLocales && restOfLocales[0]}
-            scroll={false}
-          >
-            <div className='rounded-md hover:rounded-md hover:border-gray-200 hover:bg-gray-200 dark:border-slate-200 dark:bg-slate-200 dark:hover:bg-white dark:hover:border-white p-2 transform transition duration-300 hover:cursor-pointer mx-1'>
-              <motion.div key='es' whileTap={{ rotate: 45 }}>
-                <HiTranslate className='text-2xl cursor-pointer text-gray-800' />
-              </motion.div>
-            </div>
-          </Link>
+        <Tooltip content='es → en' placement='bottom'>
+          <div className='rounded-md hover:rounded-md hover:border-gray-200 hover:bg-gray-200 dark:border-slate-200 dark:bg-slate-200 dark:hover:bg-white dark:hover:border-white p-2 transform transition duration-300 hover:cursor-pointer mx-1'>
+            <motion.div key='es' whileTap={{ rotate: 45 }}>
+              <HiTranslate className='text-2xl cursor-pointer text-gray-800' />
+            </motion.div>
+          </div>
         </Tooltip>
       ) : (
-        <Tooltip content='en → es' placement='bottom' trigger='hover'>
-          <Link
-            href={CheckValidPath(pathname)}
-            locale={restOfLocales && restOfLocales[0]}
-            scroll={false}
-          >
-            <div className='rounded-md hover:rounded-md hover:border-gray-200 hover:bg-gray-200 dark:border-slate-200 dark:bg-slate-200 dark:hover:bg-white dark:hover:border-white p-2 transform transition duration-300 hover:cursor-pointer mx-1'>
-              <motion.div key='es' whileTap={{ rotate: 45 }}>
-                <HiTranslate className='text-2xl cursor-pointer text-gray-800' />
-              </motion.div>
-            </div>
-          </Link>
+        <Tooltip content='en → es' placement='bottom'>
+          <div className='rounded-md hover:rounded-md hover:border-gray-200 hover:bg-gray-200 dark:border-slate-200 dark:bg-slate-200 dark:hover:bg-white dark:hover:border-white p-2 transform transition duration-300 hover:cursor-pointer mx-1'>
+            <motion.div key='en' whileTap={{ rotate: 45 }}>
+              <HiTranslate className='text-2xl cursor-pointer text-gray-800' />
+            </motion.div>
+          </div>
         </Tooltip>
       )}
-    </>
+    </Link>
   )
 }
 
