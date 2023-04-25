060739e02d18a71ec993d17b477d922d69bc18e2
juancgalueweb <juancgalue@icloud.com>
2023-04-25 18:31:15 -0400

    Add a new certification: Scrum Foundation. Update all dependencies

--- src/pages/components/AllDiplomas.tsx
+++ src/pages/components/AllDiplomas.tsx
@@ -3,13 +3,15 @@ import Docker2022 from './Diplomas/Docker2022'
 import NextJS2022 from './Diplomas/NextJS2022'
 import PFE2020 from './Diplomas/PFE2020'
 import ScriptingInPython2020 from './Diplomas/ScriptingInPython2020'
+import ScrumFoundation2023 from './Diplomas/ScrumFoundation2023'
 import TypeScript2023 from './Diplomas/TypeScript2023'
 import WDFE2021 from './Diplomas/WDFE2021'
 
 const AllDiplomas = () => {
   return (
     <>
-      <TypeScript2023 latest={true} />
+      <ScrumFoundation2023 latest />
+      <TypeScript2023 latest={false} />
       <NextJS2022 latest={false} />
       <Docker2022 />
       <CodingDojo2021 />
