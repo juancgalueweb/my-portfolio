330c0b6ab2cf7c4f17c013fce6d06fbbd98f33ed
juancgalueweb <juancgalue@icloud.com>
2023-08-04 01:45:53 -0400

    feat: :sparkles: Add the word 'Deployment' next to status indicator

--- src/pages/components/Badges/OfflineStatus.tsx
+++ src/pages/components/Badges/OfflineStatus.tsx
@@ -1,9 +1,18 @@
+import useTranslation from '@/hooks/useTranslation'
+
 const OfflineStatus = () => {
+  const { t } = useTranslation()
+
   return (
-    <span className='relative flex h-4 w-4'>
-      <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75'></span>
-      <span className='relative inline-flex rounded-full h-4 w-4 bg-red-500'></span>
-    </span>
+    <div className='flex justify-center items-center mx-1'>
+      <span className='text-gray-900 dark:text-white mr-3'>
+        {t('DEPLOYMENT')}
+      </span>
+      <span className='relative flex h-4 w-4'>
+        <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75'></span>
+        <span className='relative inline-flex rounded-full h-4 w-4 bg-red-500'></span>
+      </span>
+    </div>
   )
 }
 
